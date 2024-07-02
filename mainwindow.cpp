0f6c2fce0d3f7068d234cf8c4f3064b4e8e04c8e
Anastasiastasia <nastya.lalaloopsy@gmail.com>
2024-07-02 15:44:17 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,31 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QColorDialog"
+#include"QString"
 #include "QKeyEvent"
-#include <QMessageBox>
+#include "QComboBox"
+#include<QTextStream>
 #include "dialog.h"
-#include "QString"
+#include"QCheckBox"
+#include<QFile>
+#include <algorithm>
+#include <string>
+using namespace std;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-        // заполнение элементами ComboBox
-        // 1 способ:
-    for(int i = 0; i < 5; i++)
-    {
-        ui->comboBox->addItem("Element " + QString::number(i));
-    }
-ui->label->setText("TEXT");
+    ui->label->setText("Insert number");
+    //QFont font;
+    //создать шрифт, высоту-  10 пикселей
+    //  QFont font( "Arial", 10);
     QFont font( "Cursive", 10, QFont::Bold);
-font.setPointSize(20);
-    ui->label->setStyleSheet("color:red;");
-ui->label->setFixedHeight(30);
- ui->label->setFixedWidth(100);
- ui->label->setAlignment(Qt::AlignTop | Qt::AlignCenter);
- ui->label->setContentsMargins(1, 1, 2, 2);
- this->setContextMenuPolicy(Qt::CustomContextMenu);
+        // QFont font( "Cursive", 10);
+    //QFont font( "helvetica", 40 );
+    //QFont font= QFont("SansSerif");
+    font.setPointSize(20); // размер шрифта
+    // Установить цвет текста:
+    ui->label->setStyleSheet("color:blue;");
+
+    ui->label->setFixedHeight(30);//высота поля label
+    ui->label->setFixedWidth(100);//ширина поля label
+    //размещение текста:
+    ui->label->setAlignment(Qt::AlignTop | Qt::AlignCenter);
+    QFile fileIn("D:\\1.txt");
+    if (!fileIn.open(QIODevice::ReadOnly|QIODevice::Text)) {
+        qWarning("The file does not exist");
+        return;
+    }
+    QTextStream in(&fileIn);
+    while(!in.atEnd())
+    {
+        QString line=in.readLine();
+        ui->comboBox->addItem(line);
+    }
+    fileIn.close();
+
 
 }
 void MainWindow::keyPressEvent(QKeyEvent *e)
@@ -86,23 +104,113 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-void MainWindow::on_pushButton_1_clicked()
+void MainWindow::on_pushButton_clicked()
 {
-    QColorDialog *dialog = new QColorDialog(this);
-    QColor color=  dialog->getColor();
-    QVariant variant= color;
-    QString colcode = variant.toString();
-    // установка выбранного цвета для текста и зеленый (хаки) для фона:
-    ui->label->setStyleSheet("QLabel { background-color :rgb(100, 100, 0) ; color :"+colcode+"; }");
+
+   if(ui->checkBox->isChecked())//если 10с/с
+
+      {
+        qlonglong num;
+            QString S1=ui->textEdit->toPlainText();
+            num=S1.toInt();
+            QString binary=QString::number(num,2);
+        QString octal=QString::number(num,8);
+        QString hex=QString::number(num,16);
+        ui->textEdit_2->setText("Binary:"+binary+"\nOctal:"+octal+"\nHex:"+hex);
+      }
+        if(ui->checkBox_2->isChecked())//если 2 с/с
+        {
+          qlonglong num;
+          QString S1=ui->textEdit->toPlainText();
+          num=S1.toInt();
+          qlonglong decimalNumber = 0;
+          int power = 0;
+          while (num > 0)
+          {
+              qlonglong remainder = num % 10;
+              decimalNumber += remainder * pow(2, power);
+              num /= 10;
+              power++;
+          }
+          QString norm=QString::number(decimalNumber,10);
+          QString octal=QString::number( decimalNumber,8);
+          QString hex=QString::number(decimalNumber,16);
+        ui->textEdit_2->setText("Dec:"+norm+"\nOctal:"+octal+"\nHex:"+hex);
+        }
+         if(ui->checkBox_3->isChecked())//если 8с/с
+        {
+            QString S1=ui->textEdit->toPlainText();
+
+                  qlonglong decimalNumber = 0;
+                int power = 0;
+
+   // Convert octal number to decimal number
+   for (int i = S1.length() - 1; i >= 0; i--)
+   {
+    int digit = S1[i].digitValue();
+    decimalNumber += digit * qPow(8, power);
+    power++;
+    }
+   QString norm=QString::number(decimalNumber,10);
+   QString binary=QString::number( decimalNumber,2);
+   QString hex=QString::number(decimalNumber,16);
+    ui->textEdit_2->setText("Dec:"+norm+"\nBinary:"+binary+"\nHex:"+hex);
+     }
+    if(ui->checkBox_4->isChecked())//если 16 с/с
+    {
+         QString S1=ui->textEdit->toPlainText();
+         qlonglong decimalNumber=S1.toLongLong(nullptr,16);
+          QString norm=QString::number(decimalNumber,10);
+        QString binary=QString::number( decimalNumber,2);
+         QString octal=QString::number( decimalNumber,8);
+        ui->textEdit_2->setText("Norm:"+norm+"\nBinary:"+binary+"\nOctal:"+octal);
+
+    }
 }
+
+
+
+
 void MainWindow::on_pushButton_2_clicked()
 {
-    // убрать настройки цвета фона и текста для label
-    ui->label->setStyleSheet("*");
-    // 2 способ, используя палитру:
+    QString text=ui->comboBox->currentText();
+    ui->textEdit->setText(text);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QFile file("D:\\2.txt");
+    if (!file.open(QIODevice::WriteOnly|QIODevice::Text)) {
+        qWarning("The file2 does not exist");
+        return;
+    }
+
+    QTextStream out(&file);
+
+    while(!out.atEnd())
+    {
+        QString text=ui->textEdit_2->toPlainText();
+        out<<text;
+
+    }
+
+    file.close();
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    //создание модального окна:
+    Dialog dlg;
+    dlg.setModal(true);
+    dlg.exec();
+}
+
+
+void MainWindow::on_pushButton_5_clicked()
+{
     QPalette pal;
-    //установить желтый цвет фона вокруг текста
+        //установить желтый цвет фона вокруг текста
     pal.setColor(ui->label->backgroundRole(), Qt::yellow);
     //установки цветов RGBA (то есть цвета RGB со значением Alpha для прозрачности)
     //pal.setColor(ui->label->backgroundRole(),QColor(255, 100, 100, 255));
@@ -110,96 +218,28 @@ void MainWindow::on_pushButton_2_clicked()
     ui->label->setAutoFillBackground(true) ;
     // установить синий цвет текста:
     pal.setColor(ui->label->foregroundRole(), Qt::blue);
-    // установить цвет красный  текста (2 способ):
-    //  pal.setColor (QPalette::WindowText, Qt::red);
+
 
     // установить желтый цвет фона вокруг текста, используя QBrush:
     // CrossPattern -  заливка решёткой (клеткой):
     pal.setBrush(QPalette::Window, QBrush(QColor(255, 255, 100, 255), Qt::CrossPattern));
-    // SolidPattern -  сплошная заливка :
-    // pal.setBrush(QPalette::Window, QBrush(QColor(255, 255, 100, 255), Qt::SolidPattern));
     ui->label->setPalette(pal);
 }
-
-
-void MainWindow::on_MainWindow_customContextMenuRequested(const QPoint &pos)
+void MainWindow::on_checkBox_clicked()
 {
-    this->setContextMenuPolicy(Qt::CustomContextMenu);
-    QMenu *pMenu = new QMenu(this);
 
-    QAction * pNewTask = new QAction (tr ("Элемент 1"), this);
-    QAction * pEditTask = new QAction (tr ("Элемент 1"), this);
-    // Для текста на английском:
-    //QAction * pEditTask= new QAction ("Удуьуте 2", this);
-
-    pNewTask->setData(1);
-    pEditTask->setData(2);
-
-    // Добавить объект QAction в меню
-    pMenu->addAction(pNewTask);
-    pMenu->addAction(pEditTask);
-        // Подключаем сигнал правой кнопки мыши
-    connect(pNewTask, SIGNAL(triggered()), this, SLOT(onTaskBoxContextMenuEvent()));
-    connect(pEditTask, SIGNAL(triggered()), this, SLOT(onTaskBoxContextMenuEvent()));
-
-    // Отображение меню по правому клику мыши
-    pMenu->exec(cursor().pos());
-
-    // Освободить память
-    QList<QAction*> list = pMenu->actions();
-    foreach (QAction* pAction, list) delete pAction;
-    delete pMenu;
-}
-void MainWindow::onTaskBoxContextMenuEvent()
-{
-    // this-> sender () является сигнализатором QAction:
-    QAction * pEven = qobject_cast <QAction *> (this-> sender ());
-
-    // Получить тип отправляемой информации (1 или 2) ( 1: Элемент 1 2: Элемент 2)
-    int iType = pEven->data().toInt();
-
-    switch (iType)
-    {
-    case 1:
-        //устанавливает заголовки и выводится текст из pEven
-        QMessageBox::about(this, "Element 1", pEven->text());
-        break;
-    case 2:
-        QMessageBox::about(this, "Element 2", pEven->text());
-        break;
-    default:
-        break;
-    }
-}
-
-
-
-void MainWindow::on_pushButton_3_clicked()
-{
-        //создание модального окна:
-        Dialog dlg;
-        dlg.setModal(true);
-        dlg.exec();
 
 }
 
 
-void MainWindow::on_pushButton_4_clicked()
+void MainWindow::on_checkBox_2_clicked()
 {
-    //чтение из текстового пoля нового элемента для списка и
-    // добавление в список этого элемента
 
-    QString val=(ui->textEdit)->toPlainText();
-    if (val!="")
-        ui->comboBox->addItem(val);
 }
 
 
-void MainWindow::on_pushButton_5_clicked()
+void MainWindow::on_checkBox_4_clicked()
 {
-
-    //вывод в текстовой поле текущего элемента (выбранного) из списка
-    ui->textEdit->setText(ui->comboBox->currentText());
 
 }
 
